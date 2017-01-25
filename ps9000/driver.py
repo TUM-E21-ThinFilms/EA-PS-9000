d338768eb8d0f4b9eb4d0ae109f359aea7b8b722
Alexander Book <alexander.book@frm2.tum.de>
2017-01-25 17:29:16 +0100

    Adding protocol, driver and factory

---
+++ ps9000/driver.py
@@ -0,0 +1,74 @@
+# Copyright (C) 2016, see AUTHORS.md
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from slave.driver import Driver, Command
+from slave.types import Mapping, Float, String, Integer, Boolean, SingleType
+from protocol import EAPS9000Protocol
+
+class EAPS9000Driver(Driver):
+
+    def __init__(self, transport, protocol=None):
+        if protocol is None:
+            protocol = EAPS9000Protocol()
+        
+        self.thread = None
+        
+        super(EAPS9000Driver, self).__init__(transport, protocol)
+
+    def _write(self, message):
+        self._protocol.write(self._transport, message)
+
+    def _query(self, message):
+        return self._protocol.query(self._transport, message)
+
+    def get_identification(self):
+        return self._query('*IDN?')
+
+    def reset(self):
+        self._write('*RST')
+
+    def set_voltage(self, voltage):
+        if not isinstance(voltage, (long, int)):
+            raise ValueError("Expected voltage to be an integer")
+
+        self._write('VOLT '+ str(voltage))
+
+    def get_voltage(self):
+        return float(self._query('VOLT?'))
+
+    def set_current(self, current):
+        if not isinstance(current, (long, int)):
+            raise ValueError("Expected current to be an integer")
+
+        self._write('CURR '+ str(current))
+
+    def get_current(self):
+        return float(self._query('CURR?'))
+
+    def measure_voltage(self):
+        return float(self._query('MEAS:VOLT?'))
+
+    def measure_current(self):
+        return float(self._query('MEAS:CURR?'))
+
+    def set_output(self, bool):
+        if not isinstance(bool, boolean):
+            raise ValueError("Expected bool to be a boolean value")
+
+        self._write('OUTP '+ str(int(bool)))
+
+    def get_output(self):
+        return bool(self._query('OUTP?'))
+
---
+++ ps9000/protocol.py
@@ -0,0 +1,82 @@
+# Copyright (C) 2016, see AUTHORS.md
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import slave
+import logging
+import time
+import e21_util
+
+from slave.protocol import Protocol
+from slave.transport import Timeout
+
+from e21_util.lock import InterProcessTransportLock
+from e21_util.error import CommunicationError
+
+class EAPS9000Protocol(Protocol):
+    def __init__(self, logger=None):
+
+        if logger is None:
+            logger = logging.getLogger(__name__)
+            logger.addHandler(logging.NullHandler())
+
+        self.logger = logger
+
+    def set_logger(self, logger):
+        self.logger = logger
+
+    def send_message(self, transport, message):        
+
+        try:
+            transport.write(message + chr(0xa))
+        except slave.transport.Timeout:
+            raise CommunicationError('Received timeout.')
+
+    def read_response_frame(self, transport, retries=4):
+
+        try:
+            last = ord(transport.read_bytes(1))
+        except slave.transport.Timeout:
+            transport.write(chr(Message.CHAR_NAK))
+            return self.read_response_frame(transport, retries - 1)
+
+    
+    def read_response(self, transport, max_frames=2):
+        try:
+            response = transport.read_until("\r\n")
+        except slave.transport.Timeout:
+            raise CommunicationError('Could not read response: No delemiter CR LF received')
+
+        return response
+
+    def query(self, transport, msg):
+        with InterProcessTransportLock(transport):
+            self.logger.debug('Query message "%s"', msg)
+            self.send_message(transport, msg)
+            response = self.read_response(transport)
+            return response
+
+    def clear(self, transport):
+        with InterProcessTransportLock(transport):
+            self.logger.debug("Clearing buffer...")
+            try:
+                while True:
+                    transport.read_bytes(25)
+            except:
+                pass
+
+    def write(self, transport, msg):
+        with InterProcessTransportLock(transport):
+            self.logger.debug('Send message "%s"', msg)
+            self.send_message(transport, msg)
