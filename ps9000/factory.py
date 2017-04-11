55aaa0cfad565dbb7bd59561231d5238414422d9
Alexander Book <alexander.book@frm2.tum.de>
2017-04-11 11:06:06 +0200

    Chaning import of logging to log

--- ps9000/factory.py
+++ ps9000/factory.py
@@ -14,7 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-from e21_util.logging import get_sputter_logger
+from e21_util.log import get_sputter_logger
 from e21_util.transport import Serial
 from protocol import EAPS9000Protocol
 from driver import EAPS9000Driver
